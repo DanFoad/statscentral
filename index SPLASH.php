cef66067e13ba915ba62fbef4352e0d413c644aa
Dan Foad <danielfoad@gmail.com>
2017-02-05 22:58:12 +0000

    Splash screen for during maintenance

---
+++ index SPLASH.php
@@ -0,0 +1,12 @@
+<body style="background-color: #2b3a4d;">
+    <img src="/img/splash.jpg" style="
+    max-width: 100%;
+    max-height: 100%;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    overflow: auto;
+    position: fixed;
+    right: 0;
+    top: 0;">
+</body>
