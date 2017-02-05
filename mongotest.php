fb3cb0c91bbb86be341776ec8147b1a7f006b5a8
Dan Foad <danielfoad@gmail.com>
2017-02-05 23:15:02 +0000

    MongoDB connection test

---
+++ mongotest.php
@@ -0,0 +1,12 @@
+<?php
+
+//////////////////////////||
+// TEST FILE - NOT IN USE ||
+//////////////////////////||
+
+    $m = new MongoDB();
+    $db = $m->selectDB("lolstats");
+
+    echo "Connection successful";
+
+?>
